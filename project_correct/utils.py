80a409d4ffd3ea2f64867814a09abdfb305eb602
Marvin Raichur <marvinraichur14@gmail.com>
2025-11-14 18:00:38 +0530

    Delete project_correct directory

--- project_correct/utils.py
+++
@@ -1,8 +0,0 @@
-def load_numbers(path):
-    with open(path, "r") as f:
-        return [int(x.strip()) for x in f.readlines()]
-
-def calculate_average(nums):
-    if not nums:
-        return 0
-    return sum(nums) / len(nums)

919f0bd2cfdd513b21c0c47d7638ff0a30f015ce
Marvin Raichur <marvinraichur14@gmail.com>
2025-11-14 18:02:27 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-def load_numbers(path):
-    with open(path, "r") as f:
-        return [int(x.strip()) for x in f.readlines()]
-
-def calculate_averge(nums):   # ❌ misspelled function name
-    if not nums:
-        return 0
-    return sum(nums) / len(nums)

46999018d31d8371e78bca0003331c9724bb8b7e
marvin222-debug <marvinraichur14@gmail.com>
2026-01-27 16:08:40 +0530

    Auto-commit: Code validated and pushed

---
+++ main.py
@@ -0,0 +1,10 @@
+def multiply(a, b):   # Added comma between parameters
+    return a * b
+
+def main():
+    x = 6
+    y = 7
+    print("Product =", multiply(x, y))  # Fixed function name typo
+
+if __name__ == "__main__":
+    main()
